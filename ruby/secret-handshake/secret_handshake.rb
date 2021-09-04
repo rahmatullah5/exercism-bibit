adb6c4c50a9fd1200d56d5dfeb553b88b3485f72
Muhammad Rahmatullah <muhammad.rahmatullah@mekari.com>
2021-09-04 12:10:24 +0700

    Add solution for secret handshake

--- ruby/secret-handshake/secret_handshake.rb
+++ ruby/secret-handshake/secret_handshake.rb
@@ -7,7 +7,28 @@ To get started with TDD, see the `README.md` file in your
 =end
 
 class SecretHandshake
-  def self.command(_arr)
-    res = []
+  attr_reader :i
+  attr_accessor :arr
+
+  SECRET_CODE = [
+    'wink',
+    'double blink',
+    'close your eyes',
+    'jump'
+  ].freeze
+
+  def initialize(i)
+    @i = i.to_s(2).to_i.digits unless i.is_a? String
+    @arr = []
+  end
+
+  def commands
+    return @arr if @i.nil?
+
+    @arr = @i[0..3].each_with_index.map do |i, index|
+      SECRET_CODE[index] if i != 0
+    end.compact
+
+    @i.length > 4 ? @arr.reverse : @arr
   end
 end
