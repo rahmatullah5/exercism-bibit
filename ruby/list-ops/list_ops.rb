bbd029639cfbe4c6f82421705b44ac65160c3c43
Muhammad Rahmatullah <muhammad.rahmatullah@mekari.com>
2021-09-10 01:06:43 +0700

    Change solution to use native each instead of ruby libs

--- ruby/list-ops/list_ops.rb
+++ ruby/list-ops/list_ops.rb
@@ -9,34 +9,72 @@ To get started with TDD, see the `README.md` file in your
 require 'pry'
 class ListOps
   def self.arrays(arr)
-    arr.count
+    counter = 0
+    arr.each do
+      counter += 1
+    end
+    counter
   end
 
   def self.reverser(arr)
-    arr.reverse
+    new_arr = []
+    new_arr << arr.pop until arr.empty?
+    new_arr
   end
 
   def self.concatter(arr1, arr2)
-    arr1 + arr2
+    new_arr = []
+
+    arr1.each do |n|
+      new_arr << n
+    end
+
+    arr2.each do |n|
+      new_arr << n
+    end
+
+    new_arr
   end
 
-  def self.mapper(arr, &block)
+  def self.mapper(arr)
     return arr unless block_given?
 
-    arr.map(&block)
+    new_arr = []
+    arr.each do |v|
+      new_arr << yield(v)
+    end
+
+    new_arr
   end
 
-  def self.filterer(arr, &block)
+  def self.filterer(arr)
     return arr unless block_given?
 
-    arr.select(&block)
+    new_arr = []
+    arr.each do |v|
+      new_arr << v if yield(v)
+    end
+
+    new_arr
   end
 
   def self.sum_reducer(arr)
-    arr.inject(0, :+)
+    value = 0
+    return value unless arr
+
+    arr.each do |v|
+      value += v
+    end
+    value
   end
 
   def self.factorial_reducer(arr)
-    arr.inject(1, :*)
+    value = 1
+    return value unless arr
+
+    arr.each do |v|
+      value *= v
+    end
+    value
   end
 end
