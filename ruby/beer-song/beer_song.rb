d87ad1284c87560ce28e28ff04476842cb9e1a09
Muhammad Rahmatullah <muhammad.rahmatullah@mekari.com>
2021-09-11 00:20:08 +0700

    Optimize solution for beer song

--- ruby/beer-song/beer_song.rb
+++ ruby/beer-song/beer_song.rb
@@ -5,38 +5,33 @@ Write your code for the 'Beer Song' exercise in this file. Make the tests in
 To get started with TDD, see the `README.md` file in your
 `ruby/beer-song` directory.
 =end
-module BeerSong
-  def recite(start_position, decrement)
-    response = ""
-    decrement.times do |i|
-      if start_position > 2
-        response << <<~TEXT
-          #{start_position} bottles of beer on the wall, #{start_position} bottles of beer.
-          Take one down and pass it around, #{start_position - 1} bottles of beer on the wall.
-        TEXT
-      elsif start_position > 1
-        response << <<~TEXT
-          2 bottles of beer on the wall, 2 bottles of beer.
-          Take one down and pass it around, 1 bottle of beer on the wall.
-        TEXT
-      elsif start_position.positive?
-        response << <<~TEXT
-          1 bottle of beer on the wall, 1 bottle of beer.
-          Take it down and pass it around, no more bottles of beer on the wall.
-        TEXT
-      else
-        response << <<~TEXT
-          No more bottles of beer on the wall, no more bottles of beer.
-          Go to the store and buy some more, 99 bottles of beer on the wall.
-        TEXT
-        start_position = 100
-      end
-      start_position -= 1
-      response += "\n" if i != decrement - 1
-    end
+class BeerSong
+  def self.recite(start_position, decrement)
+    start_position.downto(start_position - decrement + 1).map { |i| populate_response(i) }.join("\n")
+  end
 
-    response
+  def self.populate_response(current_position)
+    case current_position
+    when 2
+      <<~TEXT
+        2 bottles of beer on the wall, 2 bottles of beer.
+        Take one down and pass it around, 1 bottle of beer on the wall.
+      TEXT
+    when 1
+      <<~TEXT
+        1 bottle of beer on the wall, 1 bottle of beer.
+        Take it down and pass it around, no more bottles of beer on the wall.
+      TEXT
+    when 0
+      <<~TEXT
+        No more bottles of beer on the wall, no more bottles of beer.
+        Go to the store and buy some more, 99 bottles of beer on the wall.
+      TEXT
+    else
+      <<~TEXT
+        #{current_position} bottles of beer on the wall, #{current_position} bottles of beer.
+        Take one down and pass it around, #{current_position - 1} bottles of beer on the wall.
+      TEXT
+    end
   end
 end
-
-BeerSong.extend BeerSong
